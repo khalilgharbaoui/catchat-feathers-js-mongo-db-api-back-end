8ea9db581e650217590ee981b3542ea6dd67ef8b
khalilgharbaoui <kaygeee@gmail.com>
2016-04-06 11:53:35 +0200

    added cors?

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const app = feathers()
     }
   }));
 
-  
+
 
   app.use('/dogchats', service({
     Model: db.collection('dogchats'),
@@ -40,6 +40,26 @@ const app = feathers()
       max: 100
     }
   }));
+
+
+
+  app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', function(req, res, next) {
+  // Handle the get for this route
+});
+
+app.post('/', function(req, res, next) {
+ // Handle the post for this route
+});
+
+
+
+
 
   // A basic error handler, just like Express
   // app.use(errors.handler());
